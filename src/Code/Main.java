94c3c182b9049766c4b908d1ab059d649cf82eeb
nmhtech-ai <nmhtech.ai@gmail.com>
2020-10-30 22:23:53 +0800

    Refine 30/10/2020 10:23PM

--- src/Code/Main.java
+++ src/Code/Main.java
@@ -51,12 +51,6 @@ public class Main {
 		 * This part has been moved to CmdRequest and CmdListReservation functions
 		 * The Command pattern allows us to encapsulate functions
 		 * So, clients can issue commands without knowing the actual implementations of the function*/
-		 
-		 
-//		System.out.println(SystemDate.getInstance());
-// 		Item item1 = new Item(2,new Day("13-Oct-2020"),new Day("20-Oct-2020"), 1);
-// 		Item item2 = new Item(1,new Day("13-Oct-2020"),new Day("20-Nov-2020"), 2);
-// 		Item item3 = new Item(3,new Day("13-Oct-2020"),new Day("25-Oct-2020"), 3);
 		
 		//Harvey NG V1.0 - 2020-Oct-19: Deleted the human input ID;
 		
