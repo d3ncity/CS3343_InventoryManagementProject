03bb5027acca9bcb019004cc927bdcd331a6545b
ANIRUDH T.P.V.S <anirudhtpvsp@gmail.com>
2020-11-26 17:49:06 +0800

    Update TestMain.java

--- src/TestCases/TestMain.java
+++ src/TestCases/TestMain.java
@@ -36,6 +36,17 @@ public class TestMain {
 	}
 	
 	@Test
+	public void testMain() throws IOException {
+	    System.out.println("main");
+	    String[] args = null;
+	    final InputStream original = System.in;
+	    final FileInputStream fips = new FileInputStream(new File("[path_to_file]"));
+	    System.setIn(fips);
+	    Main.main(args);
+	    System.setIn(original);
+	}
+	
+	@Test
 	public void testMain1()
 	{
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
